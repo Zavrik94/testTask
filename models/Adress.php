5b7fa9c71fe20e6fb45af271845ca9dec1db90e1
Zavrik94 <Arty.zavrik@gmail.com>
2019-12-11 19:21:31 +0200

    Create User and Adress model

---
+++ models/Adress.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "adress".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property string|null $index
+ * @property string|null $country
+ * @property string|null $city
+ * @property string|null $street
+ * @property int|null $house_number
+ * @property int|null $flat_number
+ */
+class Adress extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'adress';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'house_number', 'flat_number'], 'default', 'value' => null],
+            [['user_id', 'house_number', 'flat_number'], 'integer'],
+            [['index', 'country', 'city', 'street'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'index' => 'Index',
+            'country' => 'Country',
+            'city' => 'City',
+            'street' => 'Street',
+            'house_number' => 'House Number',
+            'flat_number' => 'Flat Number',
+        ];
+    }
+}
--- models/User.php
+++ models/User.php
@@ -2,73 +2,89 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+use Yii;
+use yii\db\Query;
+use yii\web\IdentityInterface;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string|null $login
+ * @property string|null $password
+ * @property string|null $name
+ * @property string|null $surname
+ * @property string|null $sex
+ * @property string|null $create_date
+ * @property string|null $email
+ */
+class User extends \yii\db\ActiveRecord implements IdentityInterface
 {
-    public $id;
-    public $username;
-    public $password;
-    public $authKey;
-    public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName(): string
+    {
+        return 'user';
+    }
 
     /**
      * {@inheritdoc}
+     */
+    public function rules(): array
+    {
+        return [
+            [['create_date'], 'safe'],
+            [['login', 'password', 'name', 'surname', 'sex', 'email'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels(): array
+    {
+        return [
+            'id' => 'ID',
+            'login' => 'Login',
+            'password' => 'Password',
+            'name' => 'Name',
+            'surname' => 'Surname',
+            'sex' => 'Sex',
+            'create_date' => 'Create Date',
+            'email' => 'Email',
+        ];
+    }
+
+    /**
+     * @param $id
+     *
+     * @return bool
      */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        return static::findOne($id);
     }
 
     /**
-     * {@inheritdoc}
+     * Finds an identity by the given token.
+     *
+     * @param mixed $token the token to be looked for
+     * @param mixed $type  the type of the token. The value of this parameter depends on the implementation.
+     *                     For example, [[\yii\filters\auth\HttpBearerAuth]] will set this parameter to be `yii\filters\auth\HttpBearerAuth`.
+     *
+     * @return IdentityInterface|null the identity object that matches the given token.
+     * Null should be returned if such an identity cannot be found
+     * or the identity is not in an active state (disabled, deleted, etc.)
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
-                return new static($user);
-            }
-        }
-
-        return null;
+        // TODO: Implement findIdentityByAccessToken() method.
     }
 
     /**
-     * Finds user by username
-     *
-     * @param string $username
-     * @return static|null
-     */
-    public static function findByUsername($username)
-    {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
-                return new static($user);
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * {@inheritdoc}
+     * Returns an ID that can uniquely identify a user identity.
+     * @return string|int an ID that uniquely identifies a user identity.
      */
     public function getId()
     {
@@ -76,29 +92,33 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     }
 
     /**
-     * {@inheritdoc}
+     * Returns a key that can be used to check the validity of a given identity ID.
+     * The key should be unique for each individual user, and should be persistent
+     * so that it can be used to check the validity of the user identity.
+     * The space of such keys should be big enough to defeat potential identity attacks.
+     * This is required if [[User::enableAutoLogin]] is enabled. The returned key will be stored on the
+     * client side as a cookie and will be used to authenticate user even if PHP session has been expired.
+     * Make sure to invalidate earlier issued authKeys when you implement force user logout, password change and
+     * other scenarios, that require forceful access revocation for old sessions.
+     * @see validateAuthKey()
+     * @return string a key that is used to check the validity of a given identity ID.
      */
     public function getAuthKey()
     {
-        return $this->authKey;
+        // TODO: Implement getAuthKey() method.
     }
 
     /**
-     * {@inheritdoc}
+     * Validates the given auth key.
+     * This is required if [[User::enableAutoLogin]] is enabled.
+     *
+     * @param string $authKey the given auth key
+     *
+     * @see getAuthKey()
+     * @return bool whether the given auth key is valid.
      */
     public function validateAuthKey($authKey)
     {
-        return $this->authKey === $authKey;
-    }
-
-    /**
-     * Validates password
-     *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
-     */
-    public function validatePassword($password)
-    {
-        return $this->password === $password;
+        // TODO: Implement validateAuthKey() method.
     }
 }
