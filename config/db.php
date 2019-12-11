fdad209c182704eb653bba175780c6f718a20c39
Zavrik94 <Arty.zavrik@gmail.com>
2019-12-11 18:48:01 +0200

    Connected DB

--- config/db.php
+++ config/db.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'class' => \yii\db\Connection::class,
+    'dsn' => 'pgsql:host=localhost;dbname=testtask',
+    'username' => 'testTask',
+    'password' => 'testTask',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
+    // 'enableSchemaCache' => true,
+    // 'schemaCacheDuration' => 60,
+    // 'schemaCache' => 'cache',
 ];
--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'ohAWalx4JI9m496UTS8ziHbN8dcoUlzt',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
